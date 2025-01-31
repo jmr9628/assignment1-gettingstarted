7eb134b09733ab1eb5ffbd380792a38fdb768c91
jmr9628 <jmr9628@nyu.edu>
2025-01-31 17:33:55 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -5,26 +5,26 @@
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-        if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-            answer = "pcap"
-        elif question == "Are encoding and encryption the same? - Yes/No":
-            answer = "No"
-        elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-            answer = "No"
-        elif question == "Is it possible to decode a message without a key?":
-            answer = "Yes"
-        elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-            answer = "No"
-        elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-            answer = "c89f95ba92affb1c702700b1f65636e8d32b11085977e15897703f01419e7fd1"
-        elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-            answer = "No"
-        elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-            answer = 5
-        elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-            answer = 3
-        else:
-            answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key?":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "c89f95ba92affb1c702700b1f65636e8d32b11085977e15897703f01419e7fd1"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 5
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+    else:
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
     return(answer)
     
 # Complete all the questions.
