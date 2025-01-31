67b7638526cd371b16c900bb3cd51e3ef7dcd9b9
jmr9628 <jmr9628@nyu.edu>
2025-01-31 18:09:22 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -10,9 +10,9 @@ def welcome_assignment_answers(question):
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "Is it possible to decode a message without a key?":
-        answer = "No"
+        answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
